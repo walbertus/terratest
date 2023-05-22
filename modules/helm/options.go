a1eca26c2e4440b839c572d7915e1baa7886269c
Shamil Ganiev <ganiev@pm.me>
2023-05-22 13:32:05 +0400

    add --set-json flag support for the helm module
    Signed-off-by: Shamil Ganiev <ganiev@pm.me>

--- modules/helm/options.go
+++ modules/helm/options.go
@@ -9,6 +9,7 @@ type Options struct {
 	ValuesFiles    []string            // List of values files to render.
 	SetValues      map[string]string   // Values that should be set via the command line.
 	SetStrValues   map[string]string   // Values that should be set via the command line explicitly as `string` types.
+	SetJsonValues  map[string]string   // Values that should be set via the command line in JSON format.
 	SetFiles       map[string]string   // Values that should be set from a file. These should be file paths. Use to avoid logging secrets.
 	KubectlOptions *k8s.KubectlOptions // KubectlOptions to control how to authenticate to kubernetes cluster. `nil` => use defaults.
 	HomePath       string              // The path to the helm home to use when calling out to helm. Empty string means use default ($HOME/.helm).
