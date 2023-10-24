50f2bb2e9c86ac78b4a7d65f2986bc1627b82cb4
Jerome Guionnet <jguionnet@guidewire.com>
2023-10-24 13:32:02 -0700

    Adding Remote repo support for Helm

--- modules/helm/template.go
+++ modules/helm/template.go
@@ -72,6 +72,47 @@ func RenderTemplateE(t testing.TestingT, options *Options, chartDir string, rele
 	return RunHelmCommandAndGetStdOutE(t, options, "template", args...)
 }
 
+func RenderRemoteTemplate(t testing.TestingT, options *Options, chartURL string, releaseName string, templateFiles []string, extraHelmArgs ...string) string {
+	out, err := RenderRemoteTemplateE(t, options, chartURL, releaseName, templateFiles, extraHelmArgs...)
+	require.NoError(t, err)
+	return out
+}
+
+// RenderTemplateE runs `helm template` to render the template given the provided options and returns stdout/stderr from
+// the template command. If you pass in templateFiles, this will only render those templates.
+func RenderRemoteTemplateE(t testing.TestingT, options *Options, chartURL string, releaseName string, templateFiles []string, extraHelmArgs ...string) (string, error) {
+	// TODO: verify the charts exists and verify dependencies
+	// Now construct the args
+	// We first construct the template args
+	args := []string{}
+	if options.KubectlOptions != nil && options.KubectlOptions.Namespace != "" {
+		args = append(args, "--namespace", options.KubectlOptions.Namespace)
+	}
+	args, err := getValuesArgsE(t, options, args...)
+	if err != nil {
+		return "", err
+	}
+	// for _, templateFile := range templateFiles {
+	// 	// validate this is a valid template file
+	// 	absTemplateFile := filepath.Join(absChartDir, templateFile)
+	// 	if !strings.HasPrefix(templateFile, "charts") && !files.FileExists(absTemplateFile) {
+	// 		return "", errors.WithStackTrace(TemplateFileNotFoundError{Path: templateFile, ChartDir: absChartDir})
+	// 	}
+
+	// 	// Note: we only get the abs template file path to check it actually exists, but the `helm template` command
+	// 	// expects the relative path from the chart.
+	// 	args = append(args, "--show-only", templateFile)
+	// }
+	// deal extraHelmArgs
+	args = append(args, extraHelmArgs...)
+
+	// ... and add the name and chart at the end as the command expects
+	args = append(args, chartURL, releaseName)
+
+	// Finally, call out to helm template command
+	return RunHelmCommandAndGetStdOutE(t, options, "template", args...)
+}
+
 // UnmarshalK8SYaml is the same as UnmarshalK8SYamlE, but will fail the test if there is an error.
 func UnmarshalK8SYaml(t testing.TestingT, yamlData string, destinationObj interface{}) {
 	require.NoError(t, UnmarshalK8SYamlE(t, yamlData, destinationObj))
---
+++ test/helm_keda_example_template_test.go
@@ -0,0 +1,80 @@
+//go:build kubeall || helm
+// +build kubeall helm
+
+// **NOTE**: we have build tags to differentiate kubernetes tests from non-kubernetes tests, and further differentiate helm
+// tests. This is done because minikube is heavy and can interfere with docker related tests in terratest. Similarly, helm
+// can overload the minikube system and thus interfere with the other kubernetes tests. Specifically, many of the tests
+// start to fail with `connection refused` errors from `minikube`. To avoid overloading the system, we run the kubernetes
+// tests and helm tests separately from the others. This may not be necessary if you have a sufficiently powerful machine.
+// We recommend at least 4 cores and 16GB of RAM if you want to run all the tests together.
+
+package test
+
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	appsv1 "k8s.io/api/apps/v1"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/gruntwork-io/terratest/modules/random"
+)
+
+// This file contains examples of how to use terratest to test helm chart template logic by rendering the templates
+// using `helm template`, and then reading in the rendered templates.
+// There are two tests:
+// - TestHelmBasicExampleTemplateRenderedDeployment: An example of how to read in the rendered object and check the
+//   computed values.
+// - TestHelmBasicExampleTemplateRequiredTemplateArgs: An example of how to check that the required args are indeed
+//   required for the template to render.
+
+// An example of how to verify the rendered template object of a Helm Chart given various inputs.
+func TestHelmKedaLocalExampleTemplateRenderedDeployment(t *testing.T) {
+	t.Parallel()
+
+	// Path to the helm chart we will test
+	helmChartPath, err := filepath.Abs("../examples/helm-keda")
+	releaseName := "keda"
+	require.NoError(t, err)
+
+	// Since we aren't deploying any resources, there is no need to setup kubectl authentication or helm home.
+
+	// Set up the namespace; confirm that the template renders the expected value for the namespace.
+	namespaceName := "medieval-" + strings.ToLower(random.UniqueId())
+	logger.Logf(t, "Namespace: %s\n", namespaceName)
+
+	// Setup the args. For this test, we will set the following input values:
+	// - containerImageRepo=nginx
+	// - containerImageTag=1.15.8
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"metricsServer.replicaCount":           "999",
+			"resources.metricServer.limits.memory": "1234Mi",
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", namespaceName),
+	}
+
+	// Run RenderTemplate to render the template and capture the output. Note that we use the version without `E`, since
+	// we want to assert that the template renders without any errors.
+	// Additionally, although we know there is only one yaml file in the template, we deliberately path a templateFiles
+	// arg to demonstrate how to select individual templates to render.
+	output := helm.RenderTemplate(t, options, helmChartPath, releaseName, []string{"templates/metrics-server/deployment.yaml"})
+
+	// Now we use kubernetes/client-go library to render the template output into the Deployment struct. This will
+	// ensure the Deployment resource is rendered correctly.
+	var deployment appsv1.Deployment
+	helm.UnmarshalK8SYaml(t, output, &deployment)
+
+	// Verify the namespace matches the expected supplied namespace.
+	require.Equal(t, namespaceName, deployment.Namespace)
+
+	// Finally, we verify the deployment pod template spec is set to the expected container image value
+	var expectedMetricsServerReplica int32
+	expectedMetricsServerReplica = 999
+	deploymentMetricsServerReplica := *deployment.Spec.Replicas
+	require.Equal(t, expectedMetricsServerReplica, deploymentMetricsServerReplica)
+}
